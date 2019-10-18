1602ac16fb215dde63d8fa47aeab3e7bb5118a18
jay101uk <jerome.purrier@gmail.com>
2019-10-18 01:08:24 +0100

    in process of getting bridge domain XML working

---
+++ create_prity_xml.py
@@ -0,0 +1,9 @@
+from xml.dom import minidom
+from xml.etree import ElementTree
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
